85d24b4c7a9b809a1c356af71171e0b74de3306b
kibrian <bkiprotich39@gmail.com>
2021-12-18 01:46:26 +0300

    file running under water

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+class HelloWorld{
+    public static void main(String[] args){
+        String myName;
+        int myAge;
+         myName = "Brian";
+        myAge = 22;
+       System.out.println(myName + "is" +myAge);
+    }
+}
