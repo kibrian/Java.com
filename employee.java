08b341ba35ac58daeaa5e94b43107c66b1602112
kibrian <bkiprotich39@gmail.com>
2021-12-26 01:01:29 +0300

    working with classes

---
+++ employee.java
@@ -0,0 +1,9 @@
+public class employee {
+    int employee_Id;
+    String firsName;
+    String lastName;
+    String status;
+    String address;
+    int phoneNo;
+    int netSalary;
+}
--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,31 @@
 class HelloWorld{
     public static void main(String[] args){
-        String myName;
+
+       
+
+        /*String myName;
         int myAge;
-         myName = "Brian";
+        myName = "Brian";
         myAge = 22;
-       System.out.println(myName + "is" +myAge);
+       System.out.println("my Name is  " + myName + " and  i`m " + myAge + " years of age ")*/
+
+       employee obj = new employee();
+        int employee_Id = 4654;
+        String firstName = "Brian";
+        String lastName = "Kip";
+        String status = "single";
+        String address = "Karen";
+        int phoneNo = 575575686;
+        int netSalary = 100000;
+
+        System.out.println(obj); 
+        System.out.println(employee_Id);
+        System.out.println("FirstName -------->  "+firstName); 
+        System.out.println("LastName  -------->  "+lastName);
+        System.out.println("status  --------->  "+status);
+        System.out.println("address ------->  "+address);
+        System.out.println("phoneNo  ------->  "+phoneNo); 
+        System.out.println("netSalary ------>  "+netSalary); 
     }
+
 }
