9132e35f92e01d12ab6948642cc55df0530c03ec
kibrian <bkiprotich39@gmail.com>
2022-06-15 11:23:28 +0300

    simple core java

---
+++ Hello/Hello.java
@@ -0,0 +1,21 @@
+package Hello;
+
+public class Hello{
+
+    public static void main(String[] args){
+
+        //System.out.println("Hello Brian");
+
+        // Java arrays
+
+       String[] cars = {"jack","Jane","james","joseph","Jim"};
+
+       //System.out.println(cars.length);
+
+        //for_each loop
+       for(String i:cars){
+           System.out.println(i);
+       }
+
+    }
+}
--- employee.java
+++
@@ -1,9 +0,0 @@
-public class employee {
-    int employee_Id;
-    String firsName;
-    String lastName;
-    String status;
-    String address;
-    int phoneNo;
-    int netSalary;
-}
